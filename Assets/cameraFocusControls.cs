07acc0ba8304070639dc035d3965f4da206987b1
ianburnette <ian_burnette@yahoo.com>
2015-07-16 08:18:01 -0400

    tweening camera work

---
+++ Assets/cameraFocusControls.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFocusControls : MonoBehaviour {
+
+	public Transform player;
+	public Rigidbody playerRB, rb;
+	public float horizontalMargin;
+	public float horizontalOffset;
+	public float grossXvel, grossZvel;
+	public int facingXDirection = 0; //1=w, 2=e
+	public int facingZDirection = 0; //1=n, 2=s
+	public float currentXoffset, currentZoffset, yOffset;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		grossXvel += playerRB.velocity.x;
+//		grossZvel += playerRB.velocity.z;
+//		if (grossXvel > horizontalMargin) {
+//			facingXDirection = 1;
+//		} else if (grossXvel < -horizontalMargin) {
+//			facingXDirection = 2;
+//		}
+//
+//		if (facingXDirection == 1 && playerRB.velocity.x > 0) {
+//			rb.velocity = new Vector3(playerRB.velocity.x, rb.velocity.y, rb.velocity.z);
+//		}else if (facingXDirection == 2 && playerRB.velocity.x < 0) {
+//			rb.velocity = new Vector3(playerRB.velocity.x, rb.velocity.y, rb.velocity.z);
+//		}
+
+		transform.position = player.transform.position + new Vector3 (currentXoffset, yOffset, currentZoffset);
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawSphere(player.transform.position + new Vector3 (horizontalOffset, 0, 0), .3f);
+		Gizmos.DrawSphere(player.transform.position + new Vector3 (-horizontalOffset, 0, 0), .3f);
+	}
+}
+
---
+++ Assets/customCameraControls.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class customCameraControls : MonoBehaviour {
+
+	public Transform player, camFocus;
+
+	public Vector3[] camVectors;
+	public int cameraPosition; //1=close, 2=med, 3=far
+	public int cameraFocusDirection = 3; //1=n, 2=s, 3=w, 4=e
+	public float posChangeTime;
+	public float currentCamHeight, currentCamDist;
+
+	// Use this for initialization
+	void Start () {
+		SetCameraPosition (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AlignWithFocus ();
+		GetCameraInput ();
+		SetCamFocusPosition ();
+
+	}
+
+	void AlignWithFocus(){
+		transform.position = new Vector3(camFocus.position.x, camFocus.position.y + currentCamHeight,camFocus.position.z - currentCamDist);
+	}
+
+	void SetCamFocusPosition(){
+
+	}
+
+	void GetCameraInput(){
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			SetCameraPosition(1);
+		}if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			SetCameraPosition(2);
+		}if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			SetCameraPosition(3);
+		}
+
+	}
+
+	void SetCameraPosition(int pos){
+		iTween.ValueTo (gameObject, iTween.Hash(
+			"from", currentCamHeight,
+			"to", camVectors [pos-1].y,
+			"time", posChangeTime,
+			"onupdate", "SetY"
+			));
+		iTween.ValueTo (gameObject, iTween.Hash(
+			"from", currentCamDist,
+			"to", camVectors [pos-1].z,
+			"time", posChangeTime,
+			"onupdate", "SetZ"
+			));
+	}
+
+	void SetY(float newY){
+		currentCamHeight = newY;
+	}
+	void SetZ(float newZ){
+		currentCamDist = newZ;
+	}
+}
